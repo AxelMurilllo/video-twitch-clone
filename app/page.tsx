5bf3ff02bd4faa4aed301db8b5743b79780866a8
AxelMurilllo <axelmurillo@u.boisestate.edu>
2024-06-21 13:55:31 -0600

    Added button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Hello</p>
+    <Button size = "lg">
+      Click me
+    </Button>
   );
 }
